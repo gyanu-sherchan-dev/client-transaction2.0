fa5e984fddb0227d915fa792efd873244520ce0e
Gyanu <gyanusherchan@outlook.com>
2023-04-26 23:22:59 +1000

    transform form added

--- src/components/customForm/TransForm.js
+++ src/components/customForm/TransForm.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+const intialState = {
+  type: "",
+  name: "",
+  amount: null, // the data type you record in the form below is number and here you are passing string, conflict, hense null
+};
 
 const TransForm = () => {
-  return <div className="transForm">TransForm</div>;
+  const [form, setForm] = useState(intialState);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    //why we need initialState data.
+    //let say we have done the call api to send data to database
+    //after that reset the form
+    //to reset call setform and pass the intialState and pass value in the form input field.
+    setForm(intialState);
+  };
+
+  return (
+    <div className="transForm">
+      <Form onSubmit={handleOnSubmit}>
+        <Row className="mt-3  gap-2">
+          <Col md="2">
+            <Form.Select
+              name="type"
+              value={form.type}
+              required
+              onChange={handleOnChange}
+            >
+              <option>Choose...</option>
+              <option value="income">Income</option>
+              <option value="expenses">Expenses</option>
+            </Form.Select>
+          </Col>
+          <Col md="5">
+            <Form.Control
+              name="name"
+              placeholder="Transaction Name"
+              required
+              onChange={handleOnChange}
+              value={form.name}
+            />
+          </Col>
+          <Col md="2">
+            <Form.Control
+              onChange={handleOnChange}
+              name="amount"
+              type="number"
+              placeholder="Amount"
+              required
+              value={form.amount}
+            />
+          </Col>
+          <Col md="2">
+            <div className="d-grid">
+              <Button type="submit">Add</Button>
+            </div>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
 };
 
 export default TransForm;
